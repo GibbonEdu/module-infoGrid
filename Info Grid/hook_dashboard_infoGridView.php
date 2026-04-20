6637656b09835328b025ee540b3863027bc178f0
Ali Alam <aalam@ichk.edu.hk>
2026-04-20 10:23:10 +0800

    Info Grid: Replaced the deprecated getRoleCategory() method to use the new RoleGateway class

--- Info Grid/hook_dashboard_infoGridView.php
+++ Info Grid/hook_dashboard_infoGridView.php
@@ -19,6 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Domain\User\RoleGateway;
 use Gibbon\Module\InfoGrid\Tables\InfoGrid;
 
 $returnInt = null;
@@ -33,7 +34,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Info Grid/infoGrid_view.ph
     global $container, $autoloader;
     $autoloader->addPsr4('Gibbon\\Module\\InfoGrid\\', realpath(__DIR__).'/src');
 
-    $roleCategory = getRoleCategory($session->get('gibbonRoleIDCurrent'), $connection2);
+    $roleCategory = $container->get(RoleGateway::class)->getRoleCategory($session->get('gibbonRoleIDCurrent'));
     $canManage = isActionAccessible($guid, $connection2, '/modules/Info Grid/infoGrid_manage.php');
 
     $table = $container->get(InfoGrid::class)->create($roleCategory, $canManage);
